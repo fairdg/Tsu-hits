772060d20b2117bac8212984edf36b736ab05900
mis_hka_ <mictekaras29@gmail.com>
2025-03-08 14:24:02 +0700

    second commit

--- Paradigm/1yellow.cpp
+++ Paradigm/1yellow.cpp
@@ -2,6 +2,15 @@
 #include <functional>
 using namespace std;
 
+int* fillArray(int size, int index = 0) {
+    return (index >= size) ? new int[size] : ([&]() { 
+        int* arr = fillArray(size, index + 1);
+        cin >> arr[index];
+        return arr;  
+    })();
+}
+
+
 int* preobr(int* arr, function<bool(int)> func, int size, int& index2, int index = 0) {
     return (index >= size) ? new int[size] : ([&]() { 
         int* result = preobr(arr, func, size, index2, index + 1);
@@ -25,23 +34,18 @@ void print(int* arr, int size, int index = 0) {
     (index >= size) ? void() : (cout << arr[index] << " ", print(arr, size, index + 1));
 }
 
-int* fillArray(int size, int index = 0) {
-    return (index >= size) ? new int[size] : ([&]() { int* arr = fillArray(size, index + 1); cin >> arr[index]; return arr; })();
-}
-
 int main() {
-    int index1 = 0;
-    int index2 = 0;
     int size;
-    cout << "введи кол-во чисел ";
+    cout << "введи кол-во чисел: ";
     cin >> size;
-    
+
     int* mass = fillArray(size);
-    int mass1[size];
-    int mass2[size];
     
-    preobr(mass, [](int x) { return x % 2 == 0; }, size, mass2, index2);
-    filter(mass2, [](int x) { return x * x; }, index2, mass1, index1);
+    int index2 = 0;
+    int* mass2 = preobr(mass, [](int x) { return x % 2 == 0; }, size, index2);
+
+    int index1 = 0;
+    int* mass1 = filter(mass2, [](int x) { return x * x; }, index2, index1);
 
     cout << endl;
     print(mass, size);
@@ -51,4 +55,6 @@ int main() {
     print(mass1, index1);
 
     delete[] mass;
+    delete[] mass2;
+    delete[] mass1;
 }
